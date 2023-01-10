feef90e751a2c92a2dd8476f04254035c95f571e
vaibhavkev <vaibhavkev@gmail.com>
2023-01-10 13:04:41 +0530

    main menu

---
+++ GameDev Project/Assets/scripts/mainmenu.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class mainmenu : MonoBehaviour
+{
+    public void playgame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+    public void quitgame()
+    {
+        Application.Quit();
+    }
+
+
+}
