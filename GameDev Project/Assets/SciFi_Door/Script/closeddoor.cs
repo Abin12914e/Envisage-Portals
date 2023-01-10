3c9d6bd7209fc006e19aad31a7b3557a2b8fc5a3
vaibhavkev <vaibhavkev@gmail.com>
2023-01-10 22:23:25 +0530

    doors

---
+++ GameDev Project/Assets/SciFi_Door/Script/closeddoor.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class closeddoor : MonoBehaviour
+{
+    private bool once = true;
+
+    [SerializeField] private GameObject indicator1;
+
+    void OnTriggerEnter(Collider obj)
+    {
+        if (once)
+        {
+
+            once = false;
+
+            Destroy(indicator1);
+        }
+    }
+}
---
+++ GameDev Project/Assets/SciFi_Door/Script/door.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+using System.Collections;
+
+public class door : MonoBehaviour {
+	GameObject thedoor;
+	private bool once = true;
+    [SerializeField] private GameObject thedoor1;
+
+    [SerializeField] private GameObject indicator1;
+
+    void OnTriggerEnter ( Collider obj  ){
+        if (once)
+        {
+            
+            thedoor1.GetComponent<Animation>().Play("open");
+            once= false;
+
+            Destroy(indicator1);
+        }
+}
+
+
+}
